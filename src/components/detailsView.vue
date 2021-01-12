e54dc3fd2b2bd0de74e0a78fd4166e329a53ddd9
Helbis <filippiotrhelbig@gmail.com>
2021-01-12 22:49:42 +0100

    Adding basic pop-up window detailsView

--- src/components/detailsView.vue
+++ src/components/detailsView.vue
@@ -1,7 +1,30 @@
 <template>
     <div class="detailsView">
-        hello
-        <!-- <iframe src="WineScan/index.html" width="500px" height="800px"></iframe> -->
+        <div class="details-top">
+            <img
+                class="left_of_card"
+                src="./../../photos/blanc_big_474x774.jpg"
+                alt="wine"
+                loading="lazy">
+
+            <div class="middle_of_card">
+                <p class="wine_name">name</p>
+                <p class="wine_year">year</p>
+                <p class="wine_style">style</p>
+            </div>
+        </div>
+
+        <div class="details-main">
+            <div class="description">
+                Description
+            </div>
+
+            <div class="invoices">
+                Invoices
+            </div>
+
+            <p class="num_of_bottles">842</p>
+        </div>
     </div>
 </template>
 
