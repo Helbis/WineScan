f3ad72a29ae51bdc7844762d20c4ef8c38f92974
Helbis <filippiotrhelbig@gmail.com>
2021-01-16 13:02:15 +0100

    Added mariadb (MySQL server) in database directory

--- server.js
+++ server.js
@@ -2,10 +2,27 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// MySQL
+const mysql = require('mysql');
+let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'todoapp'
+});
+
+connection.connect(err => {
+    if (err) {
+        return console.error('error: ' + err.message);
+    }
+
+    console.log('Connected to the MySQL server.');
+});
+
+
+// express server
 app.use(express.static('./'));
 app.use(express.static('./public'));
-// app.use(express.static('./public/images'));
-
 
 app.get('/', (req, res) => {
     res.send('./index.html');
