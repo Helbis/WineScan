fd30ef0fb9be0b78d1efe9976cd54834ed34a44c
Helbis <filippiotrhelbig@gmail.com>
2021-01-19 12:14:24 +0100

    properly sending data to the user

--- server.js
+++ server.js
@@ -1,71 +1,68 @@
 const express = require('express');
-// const server = express();
-// const port = 3000;
-//
-// // MySQL https://www.npmjs.com/package/mysql#introduction
-// const mysql = require('mysql');
-// let connection = mysql.createConnection({
-//     host: 'localhost',
-//     port: '3306',
-//     user: 'Helbis',
-//     password: 'winescan',
-//     database: 'WineScan'
-// });
-//
-// connection.connect(error => {
-//     if (error) {
-//         return console.error('error: ' + error.message);
-//     }
-//
-//     console.log('Connected to the MySQL server.');
-// });
-//
-// connection.query('SELECT * FROM Volumes', (error, results, fields) => {
-//     if (error) throw error;
-//
-//     results.forEach((item, i) => {
-//         console.table({'name': item.name, 'volume': item.volume});
-//     });
-// });
-//
-// connection.end();
-//
-// // express server
-// server.use(express.static('./'));
-// server.use(express.static('./public'));
-//
-// server.get('/', (req, res, next) => {
-//     res.send('./index.html');
-//     next();
-// });
-//
-//
-// server.listen(port, () => {
-//     console.log(`Example server listening at http://localhost:${port}`);
-// });
-
-
-const app = require('express')();
-const http = require('http').Server(app);
+const server = express();
+const http = require('http').Server(server);
 const io = require('socket.io')(http);
+const mysql = require('mysql');
 
-app.use(express.static('./'));
-app.use(express.static('./public'));
 
-app.get('/', (req, res) => {
-    res.sendFile('index.html');
+const port = 3000;
+let sqlResult;
+
+// MySQL https://www.npmjs.com/package/mysql#introduction
+let connection = mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'Helbis',
+    password: 'winescan',
+    database: 'WineScan'
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
+connection.connect(error => {
+    if (error) {
+        return console.error('error: ' + error.message);
+    }
+
+    console.log('Connected to the MySQL server.');
 });
 
+connection.query('SELECT * FROM Volumes', (error, results, fields) => {
+    if (error) throw error;
+
+    console.table(results);
+    sqlResult = results;
+    // results.forEach((item, i) => {
+    //     console.table({'name': item.name, 'volume': item.volume});
+    // });
+});
+
+connection.end();
+
+// express server
+server.use(express.static('./'));
+server.use(express.static('./public'));
+
+server.get('/', (req, res, next) => {
+    res.send('./index.html');
+    next();
+});
+
+
+http.listen(port, () => {
+    console.log(`Example server listening at http://localhost:${port}`);
+});
+
+
+// Socket IO
 io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
+    console.log('😄\tuser connected');
+
+    socket.broadcast.emit('back2front', sqlResult);
+
+    socket.on('front2back', (msg) => {
+        console.log('\t\t' + msg);
     });
-});
 
-http.listen(3000, () => {
-    console.log('listening on *:3000');
+    socket.on('disconnect', () => {
+        console.log('😲\tuser disconnected');
+    });
 });
