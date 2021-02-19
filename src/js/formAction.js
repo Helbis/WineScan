3f445759552106f230c7a3e8743bf53d46803182
Helbis <filippiotrhelbig@gmail.com>
2021-02-19 23:34:05 +0100

    Added a bit of styling to the form

--- src/js/formAction.js
+++ src/js/formAction.js
@@ -1,4 +1,43 @@
-(function() {
-    'use strict';
-    console.log("SUCCESS");
-}());
+function addOption(name, datalist) {
+    console.log(`name = ${name}`);
+
+    const elem = document.getElementById(datalist);
+    const opt = document.createElement("option");
+
+    opt.value = name;
+    opt.innerHTML = name;
+
+    elem.appendChild(opt);
+}
+
+function initDatalist() {
+    // Get options for datalists
+    // const wine_name;
+    // const wine_year;
+    // const friends;
+    // const bottle_code;
+    // const localization;
+    // const order number;
+    // const supplier_name;
+    // const supplier_phone;
+    // const supplier_email;
+    const variety = {
+        1 : "Abouriou",
+        2 : "Abrustine",
+        3 : "Absinthe",
+        4 : "Acadie Blanc",
+        5 : "Acolon",
+        6 : "Agave Spirit",
+        7 : "Agiorgitiko",
+        8 : "Aglianico"
+    }
+
+    for (let key in variety) {
+        if (variety.hasOwnProperty(key)) {
+            addOption(variety[key], "variety_list");
+        }
+    }
+};
+
+
+initDatalist();
