171ad6e7bf8813b394d09a5d7e583b196e9a11ea
Helbis <filippiotrhelbig@gmail.com>
2021-03-03 19:59:57 +0100

    Made the toggling work between finished and stored wines

--- src/javascripts/functions.js
+++ src/javascripts/functions.js
@@ -1,29 +1,48 @@
 /*== Event listeners ==*/
 const stored = document.getElementById("stored");
-let storedSwitch = false;
-document.getElementById("stored").addEventListener("click", () => {
+const finished = document.getElementById("finished");
+let storedSwitch = true;
+let finishedSwitch = false;
+
+
+// TODO: Set styles
+// TODO: Probably remove github link
+stored.addEventListener("click", () => {
     if (storedSwitch) {
-        stored.style.color = "#f00";
-        storedSwitch = false;
+        return;
     } else {
-        stored.style.color = "#0f0";
+        setState(stored, true);
+        setState(finished, false);
         storedSwitch = true;
+        finishedSwitch = false;
     }
 });
 
-const finished = document.getElementById("finished");
-let finishedSwitch = false;
-document.getElementById("finished").addEventListener("click", () => {
+finished.addEventListener("click", () => {
     if (finishedSwitch) {
-        finished.style.color = "#f00";
-        finishedSwitch = false;
+        return;
     } else {
-        finished.style.color = "#0f0";
+        setState(finished, true);
+        setState(stored, false);
+        storedSwitch = false;
         finishedSwitch = true;
     }
 });
 
+setState(stored, true);
+setState(finished, false);
+console.log(storedSwitch, finishedSwitch);
+
 /*==============================*/
+
+function setState(what, state) {
+    if (state) {
+        what.style.color = "limegreen";
+    } else {
+        what.style.color = "red";
+    }
+}
+
 
 function removeBottle(code) {
     alert(code);
@@ -42,86 +61,80 @@ function addOption(name, datalist) {
     elem.appendChild(opt);
 }
 
-function activateDetails(row) {
-    const activeElems = document.querySelectorAll('.detailsView.inactive');
-    const card = activeElems[0];
-
-    // console.log(activeElems[0]);
-
-    activeElems.forEach(elm => {
-        if (elm === null) {
-            return;
-        }
-
-        elm.classList.remove('inactive');
-        elm.classList.add('active');
-    });
-
-    const query = `
-    SELECT
-        Wine.name AS "wine_name",
-        Wine_year.bottling_year AS "wine_year",
-        Wine_year.photo AS "Photo",
-        Style.name AS "style",
-        Bottle.scanned_code AS "scanned_code",
-        Wine.description AS "Description",
-        Invoice.note AS "Invoice"
-    FROM
-        Invoice INNER JOIN
-        Bottle ON Bottle.id_invoice = Invoice.id INNER JOIN
-        Wine_year ON Bottle.id_year = Wine_year.id INNER JOIN
-        Wine ON Wine_year.id_wine = Wine.id INNER JOIN
-        Style ON Wine.id_style = Style.id
-    ORDER BY Wine_year.bottling_year;`;
-
-    if (!io) {
+function populateWithData() {
+    if (window.socketIO_failed) {
         return;
+    } else {
+        const query = `
+            SELECT
+                Wine.name AS "wine_name",
+                Wine_year.bottling_year AS "wine_year",
+                Wine_year.photo AS "Photo",
+                Style.name AS "style",
+                Bottle.scanned_code AS "scanned_code",
+                Wine.description AS "Description",
+                Invoice.note AS "Invoice"
+            FROM
+                Invoice INNER JOIN
+                Bottle ON Bottle.id_invoice = Invoice.id INNER JOIN
+                Wine_year ON Bottle.id_year = Wine_year.id INNER JOIN
+                Wine ON Wine_year.id_wine = Wine.id INNER JOIN
+                Style ON Wine.id_style = Style.id
+            ORDER BY Wine_year.bottling_year;`;
+
+        const socket = io();
+
+        socket.emit('front2back', query);
+
+        socket.on('back2front', (data) => {
+            // console.table(data);
+            // console.log(data);
+
+            if (data.hasOwnProperty(row)) {
+                // console.log(data[row]);
+                const elem = card.cloneNode(true);
+
+                // Change Photo
+                card.children[0].children[0].setAttribute("src", data[row].Photo);
+
+                // Change middle info
+                // Wine name
+                card.children[0].children[1].children[0].innerHTML = data[row].wine_name;
+                // Wine Year
+                card.children[0].children[1].children[1].innerHTML = data[row].wine_year;
+                // Wine Style
+                card.children[0].children[1].children[2].innerHTML = data[row].style;
+                // Removing form
+                card.children[0].children[2].children[3].setAttribute("onclick", `removeBottle(${data[row].scanned_code})`);
+
+                // Change description
+                card.children[2].children[0].children[1].innerHTML = data[row].Description;
+
+                // Change invoice
+                card.children[2].children[1].children[1].innerHTML = data[row].Invoice;
+            }
+        });
     }
+}
 
-    const socket = io();
+function activateDetails(row) {
+    const card = document.querySelectorAll('.detailsView.inactive')[0];
 
-    socket.emit('front2back', query);
+    card.classList.remove('inactive');
+    card.classList.add('active');
 
-    socket.on('back2front', (data) => {
-        // console.table(data);
-        // console.log(data);
-
-        if (data.hasOwnProperty(row)) {
-            // console.log(data[row]);
-            const elem = card.cloneNode(true);
-
-            // Change Photo
-            card.children[0].children[0].setAttribute("src", data[row].Photo);
-
-            // Change middle info
-            // Wine name
-            card.children[0].children[1].children[0].innerHTML = data[row].wine_name;
-            // Wine Year
-            card.children[0].children[1].children[1].innerHTML = data[row].wine_year;
-            // Wine Style
-            card.children[0].children[1].children[2].innerHTML = data[row].style;
-            // Removing form
-            card.children[0].children[2].children[3].setAttribute("onclick", `removeBottle(${data[row].scanned_code})`);
-
-            // Change description
-            card.children[2].children[0].children[1].innerHTML = data[row].Description;
-
-            // Change invoice
-            card.children[2].children[1].children[1].innerHTML = data[row].Invoice;
-        }
-    });
+    populateWithData();
 }
 
 function closeDetails() {
-    const activeElems = document.querySelectorAll('.detailsView.active');
+    const card = document.querySelectorAll('.detailsView.active')[0];
 
-    activeElems.forEach(elm => {
-        if (elm === null) {
-            return;
-        }
-        elm.classList.remove('active');
-        elm.classList.add('inactive');
-    });
+    if (card === undefined) {
+        return;
+    }
+
+    card.classList.remove('active');
+    card.classList.add('inactive');
 }
 
 function scrollUp() {
