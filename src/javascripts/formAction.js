d5d90f5d7d99b446380f80cd4d4b2d9982b0d6eb
Helbis <filippiotrhelbig@gmail.com>
2021-03-12 18:12:21 +0100

    Front end checks for data done

--- src/javascripts/formAction.js
+++ src/javascripts/formAction.js
@@ -1,6 +1,3 @@
-const socket = io();
-
-
 function addOption(name, datalist) {
     if (name === undefined) { return; }
     if (datalist === undefined) { return; }
@@ -15,66 +12,79 @@ function addOption(name, datalist) {
 }
 
 function initDatalist() {
-    // Get options for datalists
-    socket.emit('front2back', 'SELECT name AS "wine_name" FROM Wine;');
-    socket.on('back2front', data => {
-        for (let key in data) {
-            if (data.hasOwnProperty(key)) {
-                addOption(data[key].wine_name, "wine_name_list");
-            }
-        }
-    });
+    if (window.socketIO_failed) {
+        return;
+    } else {
+        const socket = io();
 
-    socket.emit('front2back', 'SELECT name AS "variety" FROM Variety;');
-    socket.on('back2front', data => {
-        for (let key in data) {
-            if (data.hasOwnProperty(key)) {
-                addOption(data[key].variety, "variety_list");
+        // Get options for datalists
+        socket.emit('front2back', 'SELECT name AS "wine_name" FROM Wine;');
+        socket.on('back2front', data => {
+            for (let key in data) {
+                if (data.hasOwnProperty(key)) {
+                    addOption(data[key].wine_name, "wine_name_list");
+                }
             }
-        }
-    });
+        });
 
-    socket.emit('front2back', 'SELECT name AS "Localization" FROM Localization;');
-    socket.on('back2front', data => {
-        for (let key in data) {
-            if (data.hasOwnProperty(key)) {
-                addOption(data[key].Localization, "localization_list");
+        socket.emit('front2back', 'SELECT name AS "variety" FROM Variety;');
+        socket.on('back2front', data => {
+            for (let key in data) {
+                if (data.hasOwnProperty(key)) {
+                    addOption(data[key].variety, "variety_list");
+                }
             }
-        }
-    });
+        });
+
+        socket.emit('front2back', 'SELECT name AS "Localization" FROM Localization;');
+        socket.on('back2front', data => {
+            for (let key in data) {
+                if (data.hasOwnProperty(key)) {
+                    addOption(data[key].Localization, "localization_list");
+                }
+            }
+        });
+    }
 };
-
-
-initDatalist();
 
 
 function submitForm() {
     const form = document.querySelector('form');
     const data = Object.fromEntries(new FormData(form).entries());
 
-    console.log(data);
-    // console.log(data.volumes_options);
-
-    // You can add only
-    //  wine,
-    //  wine + wine_year,
-    //  wine + wine_year + bottle
-
-    if (data.wine_name.length === 0) {
-        alert("Zero length of wine name!");
+    if (window.socketIO_failed) {
         return;
     } else {
-        console.log("Transmittion to server . . .");
-        console.log(data);
-        socket.emit('submition', data);
+        const socket = io();
+
+        // console.log(data);
+
+        // Check if wine_name was given
+        if (data.wine_name.length === 0 || data.variety.length === 0) {
+            alert("Please check wine data");
+            return; //Without a proper wine name there is no need to send the data
+        } else {
+            // Data has wine_name and variety provided
+            // Check if year was given
+            if (data.wine_year.length !== 0) {
+                // User is trying to send both wine and year
+                if (1901 < parseInt(data.wine_year) && parseInt(data.wine_year) < 2155) {
+                    // Year is proper
+                    socket.emit('submition', data);
+                    // console.log("Sending wine and year");
+                } else {
+                    // Year is wrong
+                    alert("Year of the bottle is wrong\nshould be between 1901 and 2155");
+                    return;
+                }
+            } else {
+                // Year was not provided, send only wine data
+                // console.log("Sending data for wine only");
+                socket.emit('submition', data);
+            }
+        }
     }
-
-
-    // Check if wine name and wine year were given
-    // Then the bottle definition will be checked
-    //  * Add bottle to an existing wine_year
-
-    // Check if complete wine definition was given
-    // Check if bottle definition was given
-    // * Create new wine, wine_year and bottle
 }
+
+
+initDatalist();
